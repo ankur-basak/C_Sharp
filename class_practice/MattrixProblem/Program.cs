49f99ecdf87ee42e1e66d3c8f2ee1463cc306b1f
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2026-04-07 22:12:02 +0600

    Jagged Array Problem Solving

---
+++ class_practice/MattrixProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theory1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excutes = { "XOXOXO", "OXOXOX", "XOXOXO", "OXOXOX" };
+
+            foreach(String excute in excutes)
+            {
+                Console.WriteLine(excute);
+            }
+        }
+    }
+}

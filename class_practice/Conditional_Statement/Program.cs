bf05873163b76229b9d7d77340fc916fc082b5e5
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2026-04-07 21:59:22 +0600

    Conditonal Statement Handeling

---
+++ class_practice/Conditional_Statement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatement
+{
+        static void Main(string[] args)
+        {
+            for (int row =0; row<8; row++)
+            {
+                for (int col =0; col<8; col++)
+                {
+
+
+                }
+
+            }
+        }
+}

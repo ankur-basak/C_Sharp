41dc77148e86f828d8cf15f63c69be89eba679d3
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2026-04-07 21:54:51 +0600

    User input Ouput Operation

--- class_practice/Program.cs
+++ class_practice/Program.cs
@@ -4,112 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Account
+using System;
+
+class classLab2
 {
-    internal class Account
+    static void Main(string[] args)
     {
-        private string accountHolderName;
-        private string accountNumber;
-        private double balance;
+        Console.Write("Please enter the first number: ");
 
-        public string AccountHolderName
-        {
-            get { return accountHolderName; }
-            set { accountHolderName = value; }
-        }
+        float num1 = Convert.ToSingle(Console.ReadLine());
 
-        public string AccountNumber
-        {
-            get { return accountNumber; }
-            set { accountNumber = value; }
-        }
 
-        public double Balance
-        {
-            get { return balance; }
-            set { balance = value; }
-        }
+        Console.Write("Please enter the Second number: ");
 
-        public Account()
-        {
-            accountHolderName = " "; 
-            accountNumber = " ";
-            balance = 0.0;
-        }
+        float num2 = Convert.ToSingle(Console.ReadLine());
 
-        public Account(string name, string accNumber, double currentBalance)
-        {
-            accountHolderName = name;
-            accountNumber = accNumber;
-            balance = currentBalance;
-        }
-        public void DisplayAccountDetails()
-        {
-            Console.WriteLine("Account Holder Name: " + accountHolderName);
-            Console.WriteLine("Account Number: " + accountNumber);
-            Console.WriteLine("Balance: " + balance);
-        }
 
-        public void Deposit(double amount)
+        Console.Write("Please enter the operator (+, -, *, /): ");
+
+        char op = Convert.ToChar(Console.ReadLine());
+
+
+        if (op == '+')
         {
-            if (amount > 500)
+            Console.WriteLine("Result after summation: " + (num1 + num2));
+
+        }
+        else if (op == '-')
+        {
+            Console.WriteLine("Result after subtraction: " + (num1 - num2));
+
+        }
+        else if (op == '*')
+        {
+            Console.WriteLine("Result after multiplication: " + (num1 * num2));
+
+        }
+        else if (op == '/')
+        {
+            if (num2 != 0)
             {
-                balance += amount;
-                Console.WriteLine("Deposit successful. New balance: " + balance);
+                Console.WriteLine("Result after division: " + (num1 / num2));
 
             }
             else
             {
-                Console.WriteLine("Invalid deposit amount.");
+                Console.WriteLine("Division by zero is not allowed.");
+
             }
         }
-
-        public void Withdraw(double amount)
+        else
         {
-            if (amount > 500)
-            {
-                balance -= amount;
-                Console.WriteLine("Withdrawal successful. New balance: " + balance);
-            }
-            else
-            {
-                Console.WriteLine("Invalid withdrawal amount or insufficient funds.");
-            }
-        }
+            Console.WriteLine("Invalid operator. Please enter the correct operator");
 
-        public void Transfer(Account recipientAccount, double amount)
-        {
-            if (amount > 99)
-            {
-                balance -= amount;
-                recipientAccount.Deposit(amount);
-                Console.WriteLine("Transfer successful. New balance: " + balance);
-            }
-            else
-            {
-                Console.WriteLine("Invalid transfer amount or insufficient funds.");
-            }
-        }
-    }
-
-    class Start
-    {
-        static void Main(string[] args)
-        {
-            Account acc1 = new Account("Ankur Basak", "24657987", 4050.60);
-            Account acc2 = new Account("Ronaldo", "123456777", 5000.00);
-
-            acc1.DisplayAccountDetails();
-            acc2.DisplayAccountDetails();
-
-            acc1.Deposit(2000);
-            acc1.Withdraw(700);
-            acc1.Transfer(acc2, 400);
-
-            acc1.DisplayAccountDetails();
-            acc2.DisplayAccountDetails();
-
-            Console.ReadLine();
         }
     }
 }

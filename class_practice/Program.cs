3fb3d91e48b9869cc151abf68e2ec4b2b729436d
Ankur Basak <158720083+ankur-basak@users.noreply.github.com>
2026-04-07 21:55:42 +0600

    Using ofConditional Statement

--- class_practice/Program.cs
+++ class_practice/Program.cs
@@ -4,59 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System;
-
-class classLab2
+namespace ConditionalStatement
 {
-    static void Main(string[] args)
-    {
-        Console.Write("Please enter the first number: ");
-
-        float num1 = Convert.ToSingle(Console.ReadLine());
-
-
-        Console.Write("Please enter the Second number: ");
-
-        float num2 = Convert.ToSingle(Console.ReadLine());
-
-
-        Console.Write("Please enter the operator (+, -, *, /): ");
-
-        char op = Convert.ToChar(Console.ReadLine());
-
-
-        if (op == '+')
+        static void Main(string[] args)
         {
-            Console.WriteLine("Result after summation: " + (num1 + num2));
-
-        }
-        else if (op == '-')
-        {
-            Console.WriteLine("Result after subtraction: " + (num1 - num2));
-
-        }
-        else if (op == '*')
-        {
-            Console.WriteLine("Result after multiplication: " + (num1 * num2));
-
-        }
-        else if (op == '/')
-        {
-            if (num2 != 0)
+            for (int row =0; row<8; row++)
             {
-                Console.WriteLine("Result after division: " + (num1 / num2));
+                for (int col =0; col<8; col++)
+                {
 
-            }
-            else
-            {
-                Console.WriteLine("Division by zero is not allowed.");
+
+                }
 
             }
         }
-        else
-        {
-            Console.WriteLine("Invalid operator. Please enter the correct operator");
-
-        }
-    }
 }
